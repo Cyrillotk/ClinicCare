eadaf66889200f47e7f36cc51ccf17958537ee51
cyrillotk <tatacyri1l@gmail.com>
2026-09-04 10:45:32 +0100

    correcting views in authRoutes

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -4,6 +4,7 @@ const router = express.Router();
 
 // Login page
 router.get("/login", (req, res) => {
+    console.log("LOGIN ROUTE HIT");
     res.render("auth/login");
 });
 
@@ -12,14 +13,28 @@ router.get("/register", (req, res) => {
     res.render("auth/register");
 });
 
+// Register form submission
+router.post("/register", (req, res) => {
+    const { username, password } = req.body;
+
+    console.log("Username:", username);
+    console.log("Password:", password);
+
+    res.send(`Registration received for ${username}`);
+});
 // Dashboard
 router.get("/dashboard", (req, res) => {
-    res.render("pages/dashboard");
+    res.render("pages/dashboard", {
+        username: "ClinicCare User"
+    });
 });
-
 // Patients
 router.get("/patients", (req, res) => {
-    res.render("patients/index");
+    const patients = [];
+
+    res.render("patients/index", {
+        patients: patients
+    });
 });
 
 router.get("/patients/new", (req, res) => {
@@ -27,6 +42,9 @@ router.get("/patients/new", (req, res) => {
 });
 
 router.get("/patients/edit/:id", (req, res) => {
+    console.log("EDIT ROUTE HIT");
+    console.log("Patient ID:", req.params.id);
+
     res.render("patients/edit", {
         id: req.params.id
     });
